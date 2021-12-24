71b37e7d30d48e77794479bfb5616f52bcdaa3af
anandubale <au26199@gmail.com>
2021-12-24 20:57:32 +0530

    Added Program which check maximum value from 3 float numbers using generics

--- Program.cs
+++ Program.cs
@@ -4,32 +4,57 @@ namespace MaxUsingGenerics
 {
     class Program
 
-    {   public static int MaximumIntegerNumber(int Firstvalue, int secondvalue, int thirdvalue)
+    {
+        public static int MaximumIntegerNumber(int Firstvalue, int secondvalue, int thirdvalue)
         {
-            if(Firstvalue.CompareTo(secondvalue) > 0 && Firstvalue.CompareTo(thirdvalue) >0 || 
-                Firstvalue.CompareTo(secondvalue) >= 0 && Firstvalue.CompareTo(thirdvalue) >0 ||
-                Firstvalue.CompareTo(secondvalue) > 0 && Firstvalue.CompareTo(thirdvalue) >=0)
+            if (Firstvalue.CompareTo(secondvalue) > 0 && Firstvalue.CompareTo(thirdvalue) > 0 ||
+                Firstvalue.CompareTo(secondvalue) >= 0 && Firstvalue.CompareTo(thirdvalue) > 0 ||
+                Firstvalue.CompareTo(secondvalue) > 0 && Firstvalue.CompareTo(thirdvalue) >= 0)
             {
                 return Firstvalue;
             }
-            if(secondvalue.CompareTo(Firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) > 0 ||
-                secondvalue.CompareTo(Firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) > 0 ||
-                secondvalue.CompareTo(Firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) > 0)
+            if (secondvalue.CompareTo(Firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) > 0 ||
+                secondvalue.CompareTo(Firstvalue) >= 0 && secondvalue.CompareTo(thirdvalue) > 0 ||
+                secondvalue.CompareTo(Firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) >=0)
             {
                 return secondvalue;
             }
             if (thirdvalue.CompareTo(Firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) > 0 ||
-               thirdvalue.CompareTo(Firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) > 0 ||
-               thirdvalue.CompareTo(Firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) > 0)
+               thirdvalue.CompareTo(Firstvalue) >= 0 && thirdvalue.CompareTo(secondvalue) > 0 ||
+               thirdvalue.CompareTo(Firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) >= 0)
             {
                 return thirdvalue;
             }
             return Firstvalue;
         }
-        static void Main(string[] args)
-        {
-            Console.WriteLine(MaximumIntegerNumber(25, 30, 5));
 
+        public static double MaximumFloatNumber(double Firstvalue, double secondvalue, double thirdvalue)
+        {
+            if (Firstvalue.CompareTo(secondvalue) > 0 && Firstvalue.CompareTo(thirdvalue) > 0 ||
+                Firstvalue.CompareTo(secondvalue) >= 0 && Firstvalue.CompareTo(thirdvalue) > 0 ||
+                Firstvalue.CompareTo(secondvalue) > 0 && Firstvalue.CompareTo(thirdvalue) >= 0)
+            {
+                return Firstvalue;
+            }
+            if (secondvalue.CompareTo(Firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) > 0 ||
+                secondvalue.CompareTo(Firstvalue) >= 0 && secondvalue.CompareTo(thirdvalue) > 0 ||
+                secondvalue.CompareTo(Firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) >=0)
+            {
+                return secondvalue;
+            }
+            if (thirdvalue.CompareTo(Firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) > 0 ||
+               thirdvalue.CompareTo(Firstvalue) >= 0 && thirdvalue.CompareTo(secondvalue) > 0 ||
+               thirdvalue.CompareTo(Firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) >= 0)
+            {
+                return thirdvalue;
+            }
+            return Firstvalue;
         }
+            static void Main(string[] args)
+            {
+                Console.WriteLine(MaximumIntegerNumber(25, 30, 5));
+                Console.WriteLine(MaximumFloatNumber(25.5, 30.2, 5.3));
+            }
+
     }
 }
