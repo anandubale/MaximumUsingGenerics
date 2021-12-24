fd229320d06b0a212e6845230cb5857511d6fbbe
anandubale <au26199@gmail.com>
2021-12-24 20:11:36 +0530

    Added Program which calclate maximum number using generics

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MaxUsingGenerics
+{
+    class Program
+
+    {   public static int MaximumIntegerNumber(int Firstvalue, int secondvalue, int thirdvalue)
+        {
+            if(Firstvalue.CompareTo(secondvalue) > 0 && Firstvalue.CompareTo(thirdvalue) >0 || 
+                Firstvalue.CompareTo(secondvalue) >= 0 && Firstvalue.CompareTo(thirdvalue) >0 ||
+                Firstvalue.CompareTo(secondvalue) > 0 && Firstvalue.CompareTo(thirdvalue) >=0)
+            {
+                return Firstvalue;
+            }
+            if(secondvalue.CompareTo(Firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) > 0 ||
+                secondvalue.CompareTo(Firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) > 0 ||
+                secondvalue.CompareTo(Firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) > 0)
+            {
+                return secondvalue;
+            }
+            if (thirdvalue.CompareTo(Firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) > 0 ||
+               thirdvalue.CompareTo(Firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) > 0 ||
+               thirdvalue.CompareTo(Firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) > 0)
+            {
+                return thirdvalue;
+            }
+            return Firstvalue;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MaximumIntegerNumber(25, 30, 5));
+
+        }
+    }
+}
